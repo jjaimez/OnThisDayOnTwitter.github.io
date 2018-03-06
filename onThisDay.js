822c716c0a277001c368a3beabc0daf25385cf43
jjaimez <jacinto.jaimez@mercadolibre.com>
2018-03-06 08:19:50 -0300

    query

--- onThisDay.js
+++ onThisDay.js
@@ -4,19 +4,19 @@ document.getElementById("formm").addEventListener("submit", function(){
         year--;
         var i = -1
         var date = moment().add(i, 'years');
-        var date2 = moment().add(i, 'years').add(-1,'days');
+        var date2 = moment().add(i, 'years').add(+1,'days');
        
         var q = "@"+  document.getElementById("handle").value +" "
        
         while (year > 2007) {
-           q += "(since:"+date2.format("YYYY-MM-DD")+" AND until:"+date.format("YYYY-MM-DD")+") OR ";
+           q += "(since:"+date.format("YYYY-MM-DD")+" AND until:"+date2.format("YYYY-MM-DD")+") OR ";
             i--;
             date =  moment().add(i, 'years');
-            date2 = moment().add(i, 'years').add(-1,'days');
+            date2 = moment().add(i, 'years').add(+1,'days');
             year--;
 
         }
-        q += "(since:"+date2.format("YYYY-MM-DD")+" AND until:"+date.format("YYYY-MM-DD")+")";
+        q += "(since:"+date.format("YYYY-MM-DD")+" AND until:"+date2.format("YYYY-MM-DD")+")";
       parent.open("https://twitter.com/search?f=tweets&q="+q+"&src=typd")
 });  
 
